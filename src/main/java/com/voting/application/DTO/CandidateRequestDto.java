3a0dc79ac40943bbef149bfb07fade220ff9f9c4
Sanjay Kumar <kp19nimcet28@gmail.com>
2025-05-10 14:14:28 +0530

    adding more validation on candidate registration

--- src/main/java/com/voting/application/DTO/CandidateRequestDto.java
+++ src/main/java/com/voting/application/DTO/CandidateRequestDto.java
@@ -1,5 +1,6 @@
 package com.voting.application.DTO;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +9,27 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CandidateRequestDto {
+
+    @NotBlank(message = "Candidate name is required.")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Party name is required.")
+    @Size(min = 2, max = 50, message = "Party Name must be between 2 and 50 characters")
     private String partyName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Mobile number is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    @NotBlank(message = "Constituency is required")
+    private String constituency;
+
+    @NotNull(message = "Age is required")
+    @Min(value = 25, message = "Candidate must be at least 25 years old")
+    private Integer age;
 }
